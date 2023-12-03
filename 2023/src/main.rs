f8d52c42c16763bc070c7d7c120dc38637ab9a71
Daniel Keeney <daniel.keeney@gmail.com>
2023-12-03 10:38:29 -0700

    Refactor printing to use a Day structure

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -7,23 +7,50 @@ mod utils;
 fn main() {
     println!("Hello, world!");
 
-    println!("┌────────┐");
-    print_day("01", day1::part1("real.txt"), day1::part2("real.txt"));
-    println!("├────────┤");
-    print_day("02", day2::part1("real.txt"), day2::part2("real.txt"));
-    println!("├────────┤");
-    print_day("03", day3::part1("real.txt"), day3::part2("real.txt"));
-    println!("├────────┤");
-    print_day("04", day4::part1("test.txt"), day4::part2("test.txt"));
+    let mut days = Vec::new();
+    days.push(Day {
+        id: "01",
+        part1: || day1::part1("real.txt"),
+        part2: || day1::part2("real.txt"),
+    });
+    days.push(Day {
+        id: "02",
+        part1: || day2::part1("real.txt"),
+        part2: || day2::part2("real.txt"),
+    });
+    days.push(Day {
+        id: "03",
+        part1: || day3::part1("real.txt"),
+        part2: || day3::part2("real.txt"),
+    });
+    days.push(Day {
+        id: "04",
+        part1: || day4::part1("test.txt"),
+        part2: || day4::part2("test.txt"),
+    });
+    for (index, day) in days.iter().rev().enumerate() {
+        if index == 0 {
+            println!("┌────────┐");
+        } else {
+            println!("├────────┤");
+        }
+        print_day_struct(day);
+    }
     println!("└────────┘");
 
     println!("Farewell, world!");
 }
 
+struct Day {
+    id: &'static str,
+    part1: fn() -> i32,
+    part2: fn() -> i32,
+}
+
 // get more weird ascii art chars from here https://theasciicode.com.ar/extended-ascii-code/box-drawings-single-vertical-line-character-ascii-code-179.html
-fn print_day(day_id: &str, part1: i32, part2: i32) {
-    println!("│ Day {} │", day_id);
+fn print_day_struct(day: &Day) {
+    println!("│ Day {} │", day.id);
     println!("├────────┤");
-    println!("│ Part 1 │ {}", part1.to_string());
-    println!("│ Part 2 │ {}", part2.to_string());
+    println!("│ Part 1 │ {}", (day.part1)().to_string());
+    println!("│ Part 2 │ {}", (day.part2)().to_string());
 }
