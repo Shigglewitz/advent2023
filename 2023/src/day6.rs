e5dabded8a6ebb81d2df2eb512fa727af1753297
Daniel Keeney <daniel.keeney@gmail.com>
2023-12-05 23:51:43 -0700

    Re-implement day 6 part 2 using binary search
    - runs much faster with real.txt

--- 2023/src/day6.rs
+++ 2023/src/day6.rs
@@ -11,7 +11,7 @@ pub fn part2(file_name: &str) -> i64 {
     let input = utils::read_file("day6", file_name);
 
     let race = Race::parse_ignore_spaces(&input);
-    return race.num_ways_to_beat();
+    return race.num_ways_to_beat_efficient();
 }
 
 struct Race {
@@ -83,17 +83,68 @@ impl Race {
     fn num_ways_to_beat(&self) -> i64 {
         let mut num_ways = 0;
         for time_held in 1..self.time {
-            if (self.time - time_held) * time_held > self.distance_record {
+            if self.will_win(time_held) {
                 num_ways = num_ways + 1;
             }
         }
         return num_ways;
+    }
+
+    fn num_ways_to_beat_efficient(&self) -> i64 {
+        let left_bound = self.binary_search_left();
+        let right_bound = self.binary_search_right();
+
+        return right_bound - left_bound + 1;
+    }
+
+    fn will_win(&self, time_held: i64) -> bool {
+        return (self.time - time_held) * time_held > self.distance_record;
+    }
+
+    fn is_winning_boundary(&self, time_held: i64) -> bool {
+        return self.will_win(time_held)
+            && (!self.will_win(time_held - 1) || !self.will_win(time_held + 1));
+    }
+
+    fn binary_search_left(&self) -> i64 {
+        let mut left_bound = 0;
+        let mut right_bound = self.time / 2;
+        let mut this_try = right_bound / 2;
+
+        while !self.is_winning_boundary(this_try) {
+            if self.will_win(this_try) {
+                right_bound = this_try;
+            } else {
+                left_bound = this_try;
+            }
+            this_try = (left_bound + right_bound) / 2;
+        }
+
+        return this_try;
+    }
+
+    fn binary_search_right(&self) -> i64 {
+        let mut left_bound = self.time / 2;
+        let mut right_bound = self.time;
+        let mut this_try = (left_bound + right_bound) / 2;
+
+        while !self.is_winning_boundary(this_try) {
+            if self.will_win(this_try) {
+                left_bound = this_try;
+            } else {
+                right_bound = this_try;
+            }
+            this_try = (left_bound + right_bound) / 2;
+        }
+
+        return this_try;
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use rstest::rstest;
 
     fn test_races() -> Vec<Race> {
         return Race::parse(&utils::read_file("day6", "test.txt"));
@@ -137,5 +188,24 @@ mod test {
 
         assert_eq!(actual.time, 71530);
         assert_eq!(actual.distance_record, 940200);
+    }
+
+    #[rstest]
+    #[case(0, 2, 5)]
+    #[case(1, 4, 11)]
+    #[case(2, 11, 19)]
+    fn binary_search_tests(
+        #[case] race_index: usize,
+        #[case] left_bound: i64,
+        #[case] right_bound: i64,
+    ) {
+        let races = test_races();
+        let race = &races[race_index];
+
+        let actual_left_bound = race.binary_search_left();
+        let actual_right_bound = race.binary_search_right();
+
+        assert_eq!(actual_left_bound, left_bound);
+        assert_eq!(actual_right_bound, right_bound);
     }
 }
