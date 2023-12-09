4be5c628fd0a563e632204b916075742e64925fd
Daniel Keeney <daniel.keeney@gmail.com>
2023-12-08 22:56:16 -0700

    2023 - day 9 both parts

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -6,6 +6,7 @@ mod day5;
 mod day6;
 mod day7;
 mod day8;
+mod day9;
 mod utils;
 
 fn main() {
@@ -51,6 +52,11 @@ fn main() {
         id: "08",
         part1: || day8::part1("real.txt").to_string(),
         part2: || day8::part2("real.txt").to_string(),
+    });
+    days.push(Day {
+        id: "09",
+        part1: || day9::part1("real.txt").to_string(),
+        part2: || day9::part2("real.txt").to_string(),
     });
     for (index, day) in days.iter().rev().enumerate() {
         if index == 0 {
