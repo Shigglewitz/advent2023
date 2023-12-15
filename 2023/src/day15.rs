70b3210e8c95d0ee139915ce3833eaf787fff4a6
Daniel Keeney <daniel.keeney@gmail.com>
2023-12-14 23:18:08 -0700

    Clean up day 15 and add more tests

--- 2023/src/day15.rs
+++ 2023/src/day15.rs
@@ -2,15 +2,20 @@ use crate::utils;
 
 pub fn part1(file_name: &str) -> usize {
     let input = utils::read_file("day15", file_name);
-    return input.split(",").map(|step| hash(step)).sum();
+    return input.split(",").map(holiday_ascii_string_helper).sum();
 }
 
 pub fn part2(file_name: &str) -> usize {
     let input = utils::read_file("day15", file_name);
-    return initialization_sequence(&input);
+    let lens_boxes = LensBox::manual_arrangement_procedure(&input);
+    return lens_boxes
+        .iter()
+        .enumerate()
+        .map(|(index, lens_box)| lens_box.calculate_focusing_power(index))
+        .sum();
 }
 
-fn hash(input: &str) -> usize {
+fn holiday_ascii_string_helper(input: &str) -> usize {
     let mut total = 0;
     for byte in input.bytes() {
         total += byte as usize;
@@ -21,69 +26,94 @@ fn hash(input: &str) -> usize {
 }
 
 #[derive(Clone)]
+struct LensBox {
+    lenses: Vec<Lens>,
+}
+
+impl Default for LensBox {
+    fn default() -> LensBox {
+        return LensBox { lenses: Vec::new() };
+    }
+}
+
+impl LensBox {
+    fn manual_arrangement_procedure(input: &str) -> Vec<LensBox> {
+        let mut boxes: Vec<LensBox> = vec![LensBox::default(); 256];
+        let commands = input.split(",");
+        for command in commands {
+            if command.ends_with("-") {
+                let label = command.trim_end_matches("-");
+                let box_index = holiday_ascii_string_helper(label);
+                boxes[box_index].remove_lens(label);
+            } else {
+                let (label, focal_length_str) = command.split_once("=").unwrap();
+                let focal_length = focal_length_str.parse::<usize>().unwrap();
+                let box_index = holiday_ascii_string_helper(label);
+                boxes[box_index].add_lens(label, focal_length);
+            }
+        }
+        return boxes;
+    }
+
+    fn remove_lens(&mut self, label: &str) {
+        let mut lens_index = usize::MAX;
+        let num_lenses = self.lenses.len();
+        let label_string = label.to_string();
+        for index in 0..num_lenses {
+            if self.lenses[index].label == label_string {
+                lens_index = index;
+                break;
+            }
+        }
+        if lens_index != usize::MAX {
+            self.lenses.remove(lens_index);
+        }
+    }
+
+    fn add_lens(&mut self, label: &str, focal_length: usize) {
+        let mut lens_index = usize::MAX;
+        let num_lenses = self.lenses.len();
+        let label_string = label.to_string();
+        for index in 0..num_lenses {
+            if self.lenses[index].label == label_string {
+                lens_index = index;
+                break;
+            }
+        }
+        if lens_index == usize::MAX {
+            self.lenses.push(Lens {
+                label: label_string,
+                focal_length,
+            });
+        } else {
+            self.lenses[lens_index].focal_length = focal_length;
+        }
+    }
+
+    fn calculate_focusing_power(&self, index: usize) -> usize {
+        return self
+            .lenses
+            .iter()
+            .enumerate()
+            .map(|(lens_index, lens)| (index + 1) * (lens_index + 1) * lens.focal_length)
+            .sum();
+    }
+}
+
+#[derive(Clone)]
 struct Lens {
     label: String,
     focal_length: usize,
 }
 
-fn initialization_sequence(input: &str) -> usize {
-    let mut boxes: Vec<Vec<Lens>> = vec![Vec::new(); 256];
-    let commands = input.split(",");
-    for command in commands {
-        if command.ends_with("-") {
-            let label = command.trim_end_matches("-");
-            let box_index = hash(label);
-            let bx = &mut boxes[box_index];
-            let mut lens_index = usize::MAX;
-            let num_lenses = bx.len();
-            let label_string = label.to_string();
-            for index in 0..num_lenses {
-                if bx[index].label == label_string {
-                    lens_index = index;
-                    break;
-                }
-            }
-            if lens_index != usize::MAX {
-                bx.remove(lens_index);
-            }
-        } else {
-            let (label, focal_length_str) = command.split_once("=").unwrap();
-            let focal_length = focal_length_str.parse::<usize>().unwrap();
-            let box_index = hash(label);
-            let bx = &mut boxes[box_index];
-            let mut lens_index = usize::MAX;
-            let num_lenses = bx.len();
-            let label_string = label.to_string();
-            for index in 0..num_lenses {
-                if bx[index].label == label_string {
-                    lens_index = index;
-                    break;
-                }
-            }
-            if lens_index == usize::MAX {
-                bx.push(Lens {
-                    label: label_string,
-                    focal_length,
-                });
-            } else {
-                bx[lens_index].focal_length = focal_length;
-            }
-        }
-    }
-
-    let mut sum = 0;
-    for (box_index, bx) in boxes.iter().enumerate() {
-        for (lens_index, lens) in bx.iter().enumerate() {
-            let focusing_power = (box_index + 1) * (lens_index + 1) * lens.focal_length;
-            sum += focusing_power;
-        }
-    }
-    return sum;
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
+    use rstest::rstest;
+
+    fn test_lens_box() -> Vec<LensBox> {
+        return LensBox::manual_arrangement_procedure(&utils::read_file("day15", "test.txt"));
+    }
 
     #[test]
     fn part1_works() {
@@ -99,10 +129,49 @@ mod test {
         assert_eq!(actual, 145);
     }
 
-    #[test]
-    fn hash_works() {
-        let actual = hash("HASH");
+    #[rstest]
+    #[case("HASH", 52)]
+    #[case("rn=1", 30)]
+    #[case("cm-", 253)]
+    #[case("qp=3", 97)]
+    #[case("cm=2", 47)]
+    #[case("qp-", 14)]
+    #[case("pc=4", 180)]
+    #[case("ot=9", 9)]
+    #[case("ab=5", 197)]
+    #[case("pc-", 48)]
+    #[case("pc=6", 214)]
+    #[case("ot=7", 231)]
+    fn holiday_ascii_string_helper_tests(#[case] input: &str, #[case] expected: usize) {
+        let actual = holiday_ascii_string_helper(input);
+        assert_eq!(actual, expected);
+    }
 
-        assert_eq!(actual, 52);
+    #[test]
+    fn lens_box_initialization_sequence_works() {
+        let lens_boxes = test_lens_box();
+
+        assert_eq!(lens_boxes[0].lenses.len(), 2);
+        assert_eq!(lens_boxes[0].lenses[0].label, "rn");
+        assert_eq!(lens_boxes[0].lenses[0].focal_length, 1);
+        assert_eq!(lens_boxes[0].lenses[1].label, "cm");
+        assert_eq!(lens_boxes[0].lenses[1].focal_length, 2);
+        assert_eq!(lens_boxes[3].lenses.len(), 3);
+        assert_eq!(lens_boxes[3].lenses[0].label, "ot");
+        assert_eq!(lens_boxes[3].lenses[0].focal_length, 7);
+        assert_eq!(lens_boxes[3].lenses[1].label, "ab");
+        assert_eq!(lens_boxes[3].lenses[1].focal_length, 5);
+        assert_eq!(lens_boxes[3].lenses[2].label, "pc");
+        assert_eq!(lens_boxes[3].lenses[2].focal_length, 6);
+    }
+
+    #[rstest]
+    #[case(0, 5)]
+    #[case(3, 140)]
+    fn lens_box_calculate_focusing_power_tests(#[case] box_index: usize, #[case] expected: usize) {
+        let lens_boxes = test_lens_box();
+        let actual = lens_boxes[box_index].calculate_focusing_power(box_index);
+
+        assert_eq!(actual, expected);
     }
 }
