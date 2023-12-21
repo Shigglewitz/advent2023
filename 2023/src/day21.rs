893bda166e9f61fdd60513015842a298bca31ac0
Daniel Keeney <daniel.keeney@gmail.com>
2023-12-21 00:09:13 -0700

    attempt day 21 part 2 - still wip

--- 2023/src/day21.rs
+++ 2023/src/day21.rs
@@ -10,20 +10,23 @@ fn part1_with_input(input: &str) -> usize {
 }
 
 fn part2_with_input(input: &str) -> usize {
-    return input.len();
+    let garden = Garden::parse(input);
+    // TODO: needs better scaling to run this right
+    // return garden.calculate_plot_count_infinite(26501365);
+    return garden.calculate_plot_count_infinite(64);
 }
 
 struct Garden {
     layout: Vec<Vec<u8>>,
     start: Point,
-    width: usize,
-    height: usize,
+    width: i32,
+    height: i32,
 }
 
 #[derive(Eq, Hash, PartialEq, Clone)]
 struct Point {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
 }
 
 impl Garden {
@@ -46,13 +49,13 @@ impl Garden {
             .next()
             .flatten()
             .map(|(char_index, line_index)| Point {
-                x: char_index,
-                y: line_index,
+                x: char_index as i32,
+                y: line_index as i32,
             })
             .unwrap();
 
-        let width = layout[0].len();
-        let height = layout.len();
+        let width = layout[0].len() as i32;
+        let height = layout.len() as i32;
         return Garden {
             layout,
             start,
@@ -61,27 +64,40 @@ impl Garden {
         };
     }
 
+    fn char_at(&self, point: &Point) -> u8 {
+        let mut x = point.x % self.width;
+        if x < 0 {
+            x += self.width;
+        }
+        let mut y = point.y % self.height;
+        if y < 0 {
+            y += self.width;
+        }
+        return self.layout[y as usize][x as usize];
+    }
+
     fn get_neighbors(&self, point: &Point) -> Vec<Point> {
         let mut neighbors = Vec::new();
-        if point.x > 0 && self.layout[point.y][point.x - 1] != b'#' {
+        if point.x > 0 && self.layout[point.y as usize][point.x as usize - 1] != b'#' {
             neighbors.push(Point {
                 x: point.x - 1,
                 y: point.y,
             });
         }
-        if point.y > 0 && self.layout[point.y - 1][point.x] != b'#' {
+        if point.y > 0 && self.layout[point.y as usize - 1][point.x as usize] != b'#' {
             neighbors.push(Point {
                 x: point.x,
                 y: point.y - 1,
             });
         }
-        if point.x < self.width - 1 && self.layout[point.y][point.x + 1] != b'#' {
+        if point.x < self.width - 1 && self.layout[point.y as usize][point.x as usize + 1] != b'#' {
             neighbors.push(Point {
                 x: point.x + 1,
                 y: point.y,
             });
         }
-        if point.y < self.height - 1 && self.layout[point.y + 1][point.x] != b'#' {
+        if point.y < self.height - 1 && self.layout[point.y as usize + 1][point.x as usize] != b'#'
+        {
             neighbors.push(Point {
                 x: point.x,
                 y: point.y + 1,
@@ -107,11 +123,63 @@ impl Garden {
         }
         return plots.len();
     }
+
+    fn get_neighbors_infinite(&self, point: &Point) -> Vec<Point> {
+        let mut neighbors = Vec::new();
+        let left = Point {
+            x: point.x - 1,
+            y: point.y,
+        };
+        if self.char_at(&left) != b'#' {
+            neighbors.push(left);
+        }
+        let right = Point {
+            x: point.x + 1,
+            y: point.y,
+        };
+        if self.char_at(&right) != b'#' {
+            neighbors.push(right);
+        }
+        let up = Point {
+            x: point.x,
+            y: point.y - 1,
+        };
+        if self.char_at(&up) != b'#' {
+            neighbors.push(up);
+        }
+        let down = Point {
+            x: point.x,
+            y: point.y + 1,
+        };
+        if self.char_at(&down) != b'#' {
+            neighbors.push(down);
+        }
+
+        return neighbors;
+    }
+
+    fn calculate_plot_count_infinite(&self, num_steps: u32) -> usize {
+        let mut plots: HashSet<Point> = HashSet::new();
+        let mut next_plots: HashSet<Point> = HashSet::new();
+        plots.insert(self.start.clone());
+        for _ in 0..num_steps {
+            for plot in plots {
+                let neighbors = self.get_neighbors_infinite(&plot);
+                for neighbor in neighbors {
+                    next_plots.insert(neighbor);
+                }
+            }
+            plots = next_plots;
+            next_plots = HashSet::new();
+        }
+        return plots.len();
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use rstest::rstest;
 
     #[test]
     fn part1_works() {
@@ -124,9 +192,11 @@ mod test {
 
     #[test]
     fn part2_works() {
-        let actual = create("test.txt").solve_part2();
+        let input = utils::read_file("day21", "test.txt");
+        let garden = Garden::parse(&input);
+        let actual = garden.calculate_plot_count_infinite(6);
 
-        assert_eq!(actual, "141");
+        assert_eq!(actual, 16);
     }
 
     #[test]
@@ -145,5 +215,22 @@ mod test {
 
         assert_eq!(garden.start.x, 65);
         assert_eq!(garden.start.y, 65);
+    }
+
+    #[rstest]
+    #[case(6, 16)]
+    #[case(10, 50)]
+    #[case(50, 1594)]
+    #[case(100, 6536)]
+    // TODO: needs scaling improvements for these tests
+    // #[case(500, 167004)]
+    // #[case(1000, 668697)]
+    // #[case(5000, 16733044)]
+    fn garden_calculate_infinite_tests(#[case] num_steps: u32, #[case] expected: usize) {
+        let input = utils::read_file("day21", "test.txt");
+        let garden = Garden::parse(&input);
+        let actual = garden.calculate_plot_count_infinite(num_steps);
+
+        assert_eq!(actual, expected);
     }
 }
