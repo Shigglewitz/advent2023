9b3e173aca814957064f200fc5c1d4750d3d6cf9
Daniel Keeney <daniel.keeney@gmail.com>
2023-12-24 23:27:22 -0700

    2023 day23 part 2

--- 2023/src/day23.rs
+++ 2023/src/day23.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::create_advent_day;
 
@@ -11,11 +11,15 @@ fn part1_with_input(input: &str) -> u32 {
 
 fn part2_with_input(input: &str) -> u32 {
     let forest = Forest::parse(input);
-    return forest.traverse();
+    let graph = ForestGraph::parse(&forest);
+    let mut seen_vertices = vec![vec![false; forest.width]; forest.height];
+    return graph.depth_first_search(&Point { x: 1, y: 0 }, &mut seen_vertices, 0);
 }
 
 struct Forest {
     map: Vec<Vec<u8>>,
+    width: usize,
+    height: usize,
 }
 
 impl Forest {
@@ -24,69 +28,82 @@ impl Forest {
             .lines()
             .map(|line| line.bytes().collect::<Vec<_>>())
             .collect::<Vec<_>>();
+        let height = map.len();
+        let width = map[0].len();
 
-        return Forest { map };
+        return Forest { map, width, height };
     }
 
-    fn north_neighbor(&self, location: &Point) -> Option<Point> {
+    fn get_neighbor(
+        &self,
+        location: &Point,
+        dx: i32,
+        dy: i32,
+        allowed_slope: u8,
+        uphill_travel: bool,
+    ) -> Option<Point> {
         let neighbor = Point {
-            x: location.x,
-            y: location.y - 1,
+            x: location.x + dx,
+            y: location.y + dy,
         };
-        return match self.map[neighbor.y as usize][neighbor.x as usize] {
-            b'.' | b'^' => Some(neighbor),
-            _ => None,
-        };
+        let map_char = self.map[neighbor.y as usize][neighbor.x as usize];
+        if uphill_travel {
+            return match map_char {
+                b'#' => None,
+                _ => Some(neighbor),
+            };
+        } else {
+            return match map_char {
+                char if char == allowed_slope => Some(neighbor),
+                b'.' => Some(neighbor),
+                _ => None,
+            };
+        }
     }
-    fn south_neighbor(&self, location: &Point) -> Option<Point> {
-        let neighbor = Point {
-            x: location.x,
-            y: location.y + 1,
-        };
-        if neighbor.y >= self.map.len() as u32 {
+
+    fn north_neighbor(&self, location: &Point, uphill_travel: bool) -> Option<Point> {
+        if location.y == 0 {
             return None;
         }
-        return match self.map[neighbor.y as usize][neighbor.x as usize] {
-            b'.' | b'v' => Some(neighbor),
-            _ => None,
-        };
+        return self.get_neighbor(location, 0, -1, b'^', uphill_travel);
     }
-    fn east_neighbor(&self, location: &Point) -> Option<Point> {
-        let neighbor = Point {
-            x: location.x + 1,
-            y: location.y,
-        };
-        return match self.map[neighbor.y as usize][neighbor.x as usize] {
-            b'.' | b'>' => Some(neighbor),
-            _ => None,
-        };
+    fn south_neighbor(&self, location: &Point, uphill_travel: bool) -> Option<Point> {
+        if location.y >= self.map.len() as i32 - 1 {
+            return None;
+        }
+        return self.get_neighbor(location, 0, 1, b'v', uphill_travel);
     }
-    fn west_neighbor(&self, location: &Point) -> Option<Point> {
-        let neighbor = Point {
-            x: location.x - 1,
-            y: location.y,
-        };
-        return match self.map[neighbor.y as usize][neighbor.x as usize] {
-            b'.' | b'<' => Some(neighbor),
-            _ => None,
-        };
+    fn east_neighbor(&self, location: &Point, uphill_travel: bool) -> Option<Point> {
+        return self.get_neighbor(location, 1, 0, b'>', uphill_travel);
+    }
+    fn west_neighbor(&self, location: &Point, uphill_travel: bool) -> Option<Point> {
+        return self.get_neighbor(location, -1, 0, b'<', uphill_travel);
     }
 
-    fn get_unvisited_neighbors(&self, hike: &Hike) -> Vec<Point> {
+    fn get_possible_neighbors(&self, location: &Point, uphill_travel: bool) -> Vec<Point> {
         let mut potential_neighbors: Vec<Option<Point>> = Vec::new();
-        let north_neighbor = self.north_neighbor(&hike.location);
-        let south_neighbor = self.south_neighbor(&hike.location);
-        let east_neighbor = self.east_neighbor(&hike.location);
-        let west_neighbor = self.west_neighbor(&hike.location);
+        let north_neighbor = self.north_neighbor(location, uphill_travel);
+        let south_neighbor = self.south_neighbor(location, uphill_travel);
+        let east_neighbor = self.east_neighbor(location, uphill_travel);
+        let west_neighbor = self.west_neighbor(location, uphill_travel);
         potential_neighbors.push(north_neighbor);
         potential_neighbors.push(south_neighbor);
         potential_neighbors.push(east_neighbor);
         potential_neighbors.push(west_neighbor);
+
         return potential_neighbors
             .iter()
             .filter(|opt| opt.is_some())
             .map(|opt| opt.clone().unwrap())
+            .collect::<Vec<_>>();
+    }
+
+    fn get_unvisited_neighbors(&self, hike: &Hike) -> Vec<Point> {
+        let possible_neighbors = self.get_possible_neighbors(&hike.location, false);
+        return possible_neighbors
+            .iter()
             .filter(|neighbor| !hike.visited.contains(neighbor))
+            .map(|point| point.clone())
             .collect::<Vec<_>>();
     }
 
@@ -154,10 +171,143 @@ impl Hike {
     }
 }
 
-#[derive(PartialEq, Eq, Hash, Clone)]
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
 struct Point {
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct Edge {
+    destination: Point,
+    distance: u32,
+}
+
+struct ForestGraph {
+    adjacency_list: HashMap<Point, HashSet<Edge>>,
+    end_point: Point,
+}
+
+struct Trace {
+    starting_location: Point,
+    current_location: Point,
+    previous_location: Point,
+}
+
+impl ForestGraph {
+    fn parse(forest: &Forest) -> ForestGraph {
+        let mut adjacency_list = HashMap::new();
+        let end_point = Point {
+            x: forest.width as i32 - 2,
+            y: forest.height as i32 - 1,
+        };
+        adjacency_list.insert(Point { x: 1, y: 0 }, HashSet::new());
+        adjacency_list.insert(end_point.clone(), HashSet::new());
+        let mut traces: Vec<Trace> = Vec::new();
+        traces.push(Trace {
+            starting_location: Point { x: 1, y: 0 },
+            current_location: Point { x: 1, y: 0 },
+            previous_location: Point { x: 0, y: 0 },
+        });
+        while !traces.is_empty() {
+            let mut trace = traces.pop().unwrap();
+            let mut num_paths = 1;
+            let mut steps_taken = 0;
+            while num_paths == 1 {
+                steps_taken += 1;
+                let possible_neighbors =
+                    forest.get_possible_neighbors(&trace.current_location, true);
+                let next_steps = possible_neighbors
+                    .iter()
+                    .filter(|&point| point != &trace.previous_location)
+                    .collect::<Vec<_>>();
+                num_paths = next_steps.len();
+                if num_paths == 0 {
+                    if trace.current_location == end_point {
+                        adjacency_list
+                            .get_mut(&trace.starting_location)
+                            .unwrap()
+                            .insert(Edge {
+                                destination: end_point.clone(),
+                                distance: steps_taken,
+                            });
+                        adjacency_list.get_mut(&end_point).unwrap().insert(Edge {
+                            destination: trace.starting_location.clone(),
+                            distance: steps_taken,
+                        });
+                    } else {
+                        println!("dead end found at {:?}", trace.current_location);
+                    }
+                } else if num_paths == 1 {
+                    trace.previous_location = trace.current_location;
+                    trace.current_location = next_steps[0].clone();
+                } else if num_paths > 1 {
+                    adjacency_list
+                        .get_mut(&trace.starting_location)
+                        .unwrap()
+                        .insert(Edge {
+                            destination: trace.current_location.clone(),
+                            distance: steps_taken,
+                        });
+                    let found_vertex = adjacency_list.get_mut(&trace.current_location);
+                    if found_vertex.is_none() {
+                        adjacency_list.insert(
+                            trace.current_location.clone(),
+                            HashSet::from([Edge {
+                                destination: trace.starting_location.clone(),
+                                distance: steps_taken,
+                            }]),
+                        );
+                        for step in next_steps {
+                            traces.push(Trace {
+                                starting_location: trace.current_location.clone(),
+                                current_location: step.clone(),
+                                previous_location: trace.current_location.clone(),
+                            })
+                        }
+                    } else {
+                        found_vertex.unwrap().insert(Edge {
+                            destination: trace.starting_location.clone(),
+                            distance: steps_taken,
+                        });
+                    }
+                }
+            }
+        }
+
+        return ForestGraph {
+            adjacency_list,
+            end_point,
+        };
+    }
+
+    fn depth_first_search(
+        &self,
+        vertex: &Point,
+        seen_vertices: &mut Vec<Vec<bool>>,
+        distance: u32,
+    ) -> u32 {
+        if seen_vertices[vertex.y as usize][vertex.x as usize] {
+            return 0;
+        }
+        if vertex.eq(&self.end_point) {
+            // TODO: why is this off by one?
+            return distance - 1;
+        }
+        seen_vertices[vertex.y as usize][vertex.x as usize] = true;
+        let max = self
+            .adjacency_list
+            .get(vertex)
+            .unwrap()
+            .iter()
+            .map(|edge| {
+                self.depth_first_search(&edge.destination, seen_vertices, distance + edge.distance)
+            })
+            .max()
+            .unwrap();
+        seen_vertices[vertex.y as usize][vertex.x as usize] = false;
+        return max;
+    }
 }
 
 #[cfg(test)]
@@ -179,7 +329,7 @@ mod test {
     }
 
     #[test]
-    fn forst_parse_works() {
+    fn forest_parse_works() {
         let input = utils::read_file("day23", "test.txt");
         let forest = Forest::parse(&input);
 
@@ -193,5 +343,14 @@ mod test {
                 .as_str(),
             "#####################.#"
         );
+    }
+
+    #[test]
+    fn forest_graph_parse_works() {
+        let input = utils::read_file("day23", "test.txt");
+        let forest = Forest::parse(&input);
+        let _graph = ForestGraph::parse(&forest);
+
+        assert_eq!(1, 1);
     }
 }
