cc2e719bc443fbbad8adca9840c39d159229d9c9
Daniel Keeney <daniel.keeney@gmail.com>
2023-12-31 17:59:35 -0700

    Allow main to only run a single day if specified at command line

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,10 +1,18 @@
+use std::env;
+
 use advent2023::*;
 
 fn main() {
     println!("Hello, world!");
 
+    let args: Vec<String> = env::args().collect();
+    let pattern = if args.len() > 1 { &args[1] } else { "" };
+
     let map = advent_day_map();
-    let mut days = map.iter().collect::<Vec<_>>();
+    let mut days = map
+        .iter()
+        .filter(|(title, _)| title.matches(pattern).next().is_some())
+        .collect::<Vec<_>>();
     days.sort_by(|a, b| b.0.cmp(a.0));
     for (index, (_, day)) in days.iter().enumerate() {
         if index == 0 {
