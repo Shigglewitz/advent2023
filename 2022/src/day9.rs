cc3df58bbac1309cd088a8bc53aee8553ce366ec
Daniel Keeney <daniel.keeney@gmail.com>
2024-01-15 08:54:23 -0700

    2022 day 9 part 2

--- 2022/src/day9.rs
+++ 2022/src/day9.rs
@@ -4,13 +4,16 @@ use crate::utils;
 
 pub fn part1(file_name: &str) -> usize {
     let input = &utils::read_file("day9", file_name);
-    let mut simulation = Simulation::new(input);
+    let mut simulation = Simulation::new(input, 2);
     simulation.run();
     return simulation.num_visited_locations();
 }
 
 pub fn part2(file_name: &str) -> usize {
-    return 0;
+    let input = &utils::read_file("day9", file_name);
+    let mut simulation = Simulation::new(input, 10);
+    simulation.run();
+    return simulation.num_visited_locations();
 }
 
 struct Point {
@@ -19,51 +22,62 @@ struct Point {
 }
 
 struct Rope {
-    head: Point,
-    tail: Point,
+    knots: Vec<Point>,
+    num_knots: usize,
 }
 
 impl Rope {
     fn right(&mut self) {
-        self.head.x += 1;
+        self.knots[0].x += 1;
     }
 
     fn left(&mut self) {
-        self.head.x -= 1;
+        self.knots[0].x -= 1;
     }
 
     fn up(&mut self) {
-        self.head.y += 1;
+        self.knots[0].y += 1;
     }
 
     fn down(&mut self) {
-        self.head.y -= 1;
+        self.knots[0].y -= 1;
+    }
+
+    fn tail(&self) -> &Point {
+        return &self.knots[self.num_knots - 1];
     }
 
     fn align(&mut self) {
-        let x_delta = self.head.x - self.tail.x;
-        let y_delta = self.head.y - self.tail.y;
+        for index in 0..self.num_knots - 1 {
+            self.align_knots(index, index + 1);
+        }
+    }
+
+    fn align_knots(&mut self, head_index: usize, tail_index: usize) {
+        let x_delta = self.knots[head_index].x - self.knots[tail_index].x;
+        let y_delta = self.knots[head_index].y - self.knots[tail_index].y;
+        let tail = &mut self.knots[tail_index];
 
         match (x_delta, y_delta) {
-            (2, 0) => self.tail.x += 1,
-            (-2, 0) => self.tail.x -= 1,
-            (0, 2) => self.tail.y += 1,
-            (0, -2) => self.tail.y -= 1,
-            (1, 2) | (2, 1) => {
-                self.tail.x += 1;
-                self.tail.y += 1;
+            (2, 0) => tail.x += 1,
+            (-2, 0) => tail.x -= 1,
+            (0, 2) => tail.y += 1,
+            (0, -2) => tail.y -= 1,
+            (1, 2) | (2, 1) | (2, 2) => {
+                tail.x += 1;
+                tail.y += 1;
             }
-            (-1, 2) | (-2, 1) => {
-                self.tail.x -= 1;
-                self.tail.y += 1;
+            (-1, 2) | (-2, 1) | (-2, 2) => {
+                tail.x -= 1;
+                tail.y += 1;
             }
-            (1, -2) | (2, -1) => {
-                self.tail.x += 1;
-                self.tail.y -= 1;
+            (1, -2) | (2, -1) | (2, -2) => {
+                tail.x += 1;
+                tail.y -= 1;
             }
-            (-1, -2) | (-2, -1) => {
-                self.tail.x -= 1;
-                self.tail.y -= 1;
+            (-1, -2) | (-2, -1) | (-2, -2) => {
+                tail.x -= 1;
+                tail.y -= 1;
             }
             _ => (),
         }
@@ -92,13 +106,14 @@ struct Simulation {
 }
 
 impl Simulation {
-    fn new(input: &str) -> Self {
+    fn new(input: &str, num_knots: usize) -> Self {
         let instructions = input.lines().map(Instruction::parse).collect::<Vec<_>>();
+        let mut knots = Vec::new();
+        for _ in 0..num_knots {
+            knots.push(Point { x: 0, y: 0 })
+        }
         return Simulation {
-            rope: Rope {
-                head: Point { x: 0, y: 0 },
-                tail: Point { x: 0, y: 0 },
-            },
+            rope: Rope { knots, num_knots },
             instructions,
             visited_locations: HashSet::new(),
         };
@@ -117,7 +132,7 @@ impl Simulation {
                 function(&mut self.rope);
                 self.rope.align();
                 self.visited_locations
-                    .insert((self.rope.tail.x, self.rope.tail.y));
+                    .insert((self.rope.tail().x, self.rope.tail().y));
             }
         }
     }
@@ -134,16 +149,23 @@ mod test {
 
     #[test]
     fn part1_works() {
-        let actual = part1("test.txt");
+        let actual = part1("test1.txt");
 
         assert_eq!(actual, 13);
     }
 
     #[test]
     fn part2_works() {
-        let actual = part2("test.txt");
+        let actual = part2("test1.txt");
 
-        assert_eq!(actual, 0);
+        assert_eq!(actual, 1);
+    }
+
+    #[test]
+    fn part2_works_2() {
+        let actual = part2("test2.txt");
+
+        assert_eq!(actual, 36);
     }
 
     #[test]
@@ -156,49 +178,49 @@ mod test {
 
     #[test]
     fn right_works() {
-        let mut simulation = Simulation::new("R 2");
+        let mut simulation = Simulation::new("R 2", 2);
         simulation.run();
 
-        assert_eq!(simulation.rope.head.x, 2);
-        assert_eq!(simulation.rope.head.y, 0);
-        assert_eq!(simulation.rope.tail.x, 1);
-        assert_eq!(simulation.rope.tail.y, 0);
+        assert_eq!(simulation.rope.knots[0].x, 2);
+        assert_eq!(simulation.rope.knots[0].y, 0);
+        assert_eq!(simulation.rope.tail().x, 1);
+        assert_eq!(simulation.rope.tail().y, 0);
         assert_eq!(simulation.num_visited_locations(), 2);
     }
 
     #[test]
     fn left_works() {
-        let mut simulation = Simulation::new("L 2");
+        let mut simulation = Simulation::new("L 2", 2);
         simulation.run();
 
-        assert_eq!(simulation.rope.head.x, -2);
-        assert_eq!(simulation.rope.head.y, 0);
-        assert_eq!(simulation.rope.tail.x, -1);
-        assert_eq!(simulation.rope.tail.y, 0);
+        assert_eq!(simulation.rope.knots[0].x, -2);
+        assert_eq!(simulation.rope.knots[0].y, 0);
+        assert_eq!(simulation.rope.tail().x, -1);
+        assert_eq!(simulation.rope.tail().y, 0);
         assert_eq!(simulation.num_visited_locations(), 2);
     }
 
     #[test]
     fn up_works() {
-        let mut simulation = Simulation::new("U 2");
+        let mut simulation = Simulation::new("U 2", 2);
         simulation.run();
 
-        assert_eq!(simulation.rope.head.x, 0);
-        assert_eq!(simulation.rope.head.y, 2);
-        assert_eq!(simulation.rope.tail.x, 0);
-        assert_eq!(simulation.rope.tail.y, 1);
+        assert_eq!(simulation.rope.knots[0].x, 0);
+        assert_eq!(simulation.rope.knots[0].y, 2);
+        assert_eq!(simulation.rope.tail().x, 0);
+        assert_eq!(simulation.rope.tail().y, 1);
         assert_eq!(simulation.num_visited_locations(), 2);
     }
 
     #[test]
     fn down_works() {
-        let mut simulation = Simulation::new("D 2");
+        let mut simulation = Simulation::new("D 2", 2);
         simulation.run();
 
-        assert_eq!(simulation.rope.head.x, 0);
-        assert_eq!(simulation.rope.head.y, -2);
-        assert_eq!(simulation.rope.tail.x, 0);
-        assert_eq!(simulation.rope.tail.y, -1);
+        assert_eq!(simulation.rope.knots[0].x, 0);
+        assert_eq!(simulation.rope.knots[0].y, -2);
+        assert_eq!(simulation.rope.tail().x, 0);
+        assert_eq!(simulation.rope.tail().y, -1);
         assert_eq!(simulation.num_visited_locations(), 2);
     }
 
@@ -213,14 +235,14 @@ mod test {
     #[case("D 1;L 2", (-2, -1, -1, -1))]
     fn diagonals_work(#[case] input: &str, #[case] locations: (i32, i32, i32, i32)) {
         let new_input = input.split(";").collect::<Vec<_>>().join("\n");
-        let mut simulation = Simulation::new(&new_input);
+        let mut simulation = Simulation::new(&new_input, 2);
 
         simulation.run();
 
-        assert_eq!(simulation.rope.head.x, locations.0);
-        assert_eq!(simulation.rope.head.y, locations.1);
-        assert_eq!(simulation.rope.tail.x, locations.2);
-        assert_eq!(simulation.rope.tail.y, locations.3);
+        assert_eq!(simulation.rope.knots[0].x, locations.0);
+        assert_eq!(simulation.rope.knots[0].y, locations.1);
+        assert_eq!(simulation.rope.tail().x, locations.2);
+        assert_eq!(simulation.rope.tail().y, locations.3);
         assert_eq!(simulation.num_visited_locations(), 2);
     }
 }
