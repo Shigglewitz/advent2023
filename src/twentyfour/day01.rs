167247a42ddfdb729525b865e94c87bedaf78c49
Daniel Keeney <daniel.keeney@gmail.com>
2024-12-01 09:35:38 -0700

    2024 Day 01 both parts

--- src/twentyfour/day01.rs
+++ src/twentyfour/day01.rs
@@ -1,15 +1,65 @@
+use std::collections::HashMap;
+
 use crate::create_advent_day;
 
 create_advent_day!("2024", "01");
 
-fn part1_with_input(input: &str) -> i32 {
-    // let elves = parse_and_sort_elves(input);
+fn part1_with_input(input: &str) -> i64 {
+    let mut left: Vec<i64> = Vec::new();
+    let mut right: Vec<i64> = Vec::new();
 
-    return 0;
+    input.lines().for_each(|line| {
+        let mut split = line.split_whitespace();
+        left.push(split.next().unwrap().parse::<i64>().unwrap());
+        right.push(split.next().unwrap().parse::<i64>().unwrap());
+    });
+
+    left.sort();
+    right.sort();
+
+    let mut sum = 0;
+
+    for it in left.iter().zip(right.iter()) {
+        let (a, b) = it;
+        sum += (a-b).abs();
+    }
+    
+
+    return sum;
 }
 
-fn part2_with_input(input: &str) -> i32 {
-    // let elves = parse_and_sort_elves(input);
+fn part2_with_input(input: &str) -> i64 {
+    let mut left: Vec<i64> = Vec::new();
+    let mut right: HashMap<i64, i64> = HashMap::new();
 
-    return 0;
+    input.lines().for_each(|line| {
+        let mut split = line.split_whitespace();
+        left.push(split.next().unwrap().parse::<i64>().unwrap());
+        let right_val = split.next().unwrap().parse::<i64>().unwrap();
+        let curr_val = right.get(&right_val).unwrap_or(&0);
+        right.insert(right_val, curr_val + 1);
+    });
+
+    return left.iter().map(|location| {
+        location * right.get(location).unwrap_or(&0)
+    }).sum();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_works() {
+        let actual = create("test.txt").solve_part1();
+
+        assert_eq!("11", &actual);
+    }
+
+    #[test]
+    fn part2_works() {
+        let actual = create("test.txt").solve_part2();
+
+        assert_eq!("31", &actual);
+    }
 }
