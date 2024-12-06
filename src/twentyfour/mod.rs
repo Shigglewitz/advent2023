f9b161b608b33cd70794f44c6f5db17ef087e765
Daniel Keeney <daniel.keeney@gmail.com>
2024-12-05 23:28:40 -0700

    Set up 2024 day 08

--- src/twentyfour/mod.rs
+++ src/twentyfour/mod.rs
@@ -9,6 +9,7 @@ pub mod day04;
 pub mod day05;
 pub mod day06;
 pub mod day07;
+pub mod day08;
 
 pub fn add_twenty_four(map: &mut HashMap<String, AdventDay>) {
     map.insert("2024_01".to_owned(), day01::create("real.txt"));
@@ -18,4 +19,5 @@ pub fn add_twenty_four(map: &mut HashMap<String, AdventDay>) {
     map.insert("2024_05".to_owned(), day05::create("real.txt"));
     map.insert("2024_06".to_owned(), day06::create("real.txt"));
     map.insert("2024_07".to_owned(), day07::create("real.txt"));
+    map.insert("2024_08".to_owned(), day08::create("real.txt"));
 }
