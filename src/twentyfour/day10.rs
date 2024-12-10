649a0dc70aaa7ce70c249456e8366c79a018cc06
Daniel Keeney <daniel.keeney@gmail.com>
2024-12-09 23:31:11 -0700

    Code cleanup

--- src/twentyfour/day10.rs
+++ src/twentyfour/day10.rs
@@ -5,7 +5,22 @@ use crate::create_advent_day;
 create_advent_day!("2024", "10");
 
 fn part1_with_input(input: &str) -> i64 {
-    let heights: Vec<Vec<u32>> = input
+    let heights = parse_heights(input);
+    let trailheads: Vec<Trailhead> = parse_trailheads(&heights);
+
+    let map = Map {
+        altitudes: heights,
+        width: input.lines().into_iter().next().unwrap().len(),
+        height: input.lines().count(),
+    };
+    return trailheads
+        .iter()
+        .map(|trailhead| trailhead.score(&map))
+        .sum();
+}
+
+fn parse_heights(input: &str) -> Vec<Vec<u32>> {
+    return input
         .lines()
         .map(|line| {
             line.chars()
@@ -13,7 +28,10 @@ fn part1_with_input(input: &str) -> i64 {
                 .collect()
         })
         .collect();
-    let trailheads: Vec<Trailhead> = heights
+}
+
+fn parse_trailheads(heights: &Vec<Vec<u32>>) -> Vec<Trailhead> {
+    return heights
         .iter()
         .enumerate()
         .map(|(line_num, line)| {
@@ -33,16 +51,6 @@ fn part1_with_input(input: &str) -> i64 {
         })
         .flatten()
         .collect();
-
-    let map = Map {
-        altitudes: heights,
-        width: input.lines().into_iter().next().unwrap().len(),
-        height: input.lines().count(),
-    };
-    return trailheads
-        .iter()
-        .map(|trailhead| trailhead.score(&map))
-        .sum();
 }
 
 struct Map {
@@ -79,18 +87,8 @@ struct Trailhead {
 
 impl Trailhead {
     fn score(&self, map: &Map) -> i64 {
-        let neighbors = map.neighbors(self.x, self.y);
-        let candidates: Vec<(usize, usize)> = neighbors
-            .into_iter()
-            .filter(|(x, y)| map.altitudes[*y][*x] == 1)
-            .collect();
-        let possibilities: Vec<Option<(usize, usize)>> = candidates
-            .iter()
-            .map(|(x, y)| self.traverse(map, 1, *x, *y))
-            .flatten()
-            .collect();
-
-        return possibilities
+        return self
+            .traverse(map, 0, self.x, self.y)
             .iter()
             .filter_map(|entry| *entry)
             .collect::<HashSet<(usize, usize)>>()
@@ -107,75 +105,35 @@ impl Trailhead {
         if altitude == 9 {
             return vec![Some((x, y))];
         }
-        let neighbors = map.neighbors(x, y);
-        let candidates: Vec<(usize, usize)> = neighbors
+        return map
+            .neighbors(x, y)
             .into_iter()
             .filter(|(x, y)| map.altitudes[*y][*x] == altitude + 1)
-            .collect();
-        return candidates
-            .iter()
-            .map(|(x, y)| self.traverse(map, altitude + 1, *x, *y))
+            .map(|(x, y)| self.traverse(map, altitude + 1, x, y))
             .flatten()
             .collect();
     }
 
     fn rating(&self, map: &Map) -> i64 {
-        let neighbors = map.neighbors(self.x, self.y);
-        let candidates: Vec<(usize, usize)> = neighbors
-            .into_iter()
-            .filter(|(x, y)| map.altitudes[*y][*x] == 1)
-            .collect();
-        return candidates
-            .iter()
-            .map(|(x, y)| self.count_trails(map, 1, *x, *y))
-            .sum();
+        return self.count_trails(map, 0, self.x, self.y);
     }
 
     fn count_trails(&self, map: &Map, altitude: u32, x: usize, y: usize) -> i64 {
         if altitude == 9 {
             return 1;
         }
-        let neighbors = map.neighbors(x, y);
-        let candidates: Vec<(usize, usize)> = neighbors
+        return map
+            .neighbors(x, y)
             .into_iter()
             .filter(|(x, y)| map.altitudes[*y][*x] == altitude + 1)
-            .collect();
-        return candidates
-            .iter()
-            .map(|(x, y)| self.count_trails(map, altitude + 1, *x, *y))
+            .map(|(x, y)| self.count_trails(map, altitude + 1, x, y))
             .sum();
     }
 }
 
 fn part2_with_input(input: &str) -> i64 {
-    let heights: Vec<Vec<u32>> = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|letter| char::to_digit(letter, 10).unwrap())
-                .collect()
-        })
-        .collect();
-    let trailheads: Vec<Trailhead> = heights
-        .iter()
-        .enumerate()
-        .map(|(line_num, line)| {
-            line.iter()
-                .enumerate()
-                .filter_map(|(row_num, &height)| {
-                    if height == 0 {
-                        Some(Trailhead {
-                            x: row_num,
-                            y: line_num,
-                        })
-                    } else {
-                        None
-                    }
-                })
-                .collect::<Vec<Trailhead>>()
-        })
-        .flatten()
-        .collect();
+    let heights = parse_heights(input);
+    let trailheads = parse_trailheads(&heights);
 
     let map = Map {
         altitudes: heights,
