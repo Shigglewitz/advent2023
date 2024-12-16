f68dc6fd534e3e8e59727090e355c320f991408e
Daniel Keeney <daniel.keeney@gmail.com>
2024-12-15 22:05:59 -0700

    2024 day 15 part 1

--- src/twentyfour/day14.rs
+++ src/twentyfour/day14.rs
@@ -1,6 +1,5 @@
 use std::fs;
 
-
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use crate::create_advent_day;
@@ -23,57 +22,60 @@ fn part1_with_confines(input: &str, width: i64, height: i64) -> i64 {
         } else if y == center_height {
             acc
         } else if x < center_width && y < center_height {
-            (acc.0+1, acc.1, acc.2, acc.3)
+            (acc.0 + 1, acc.1, acc.2, acc.3)
         } else if x > center_width && y < center_height {
-            (acc.0, acc.1+1, acc.2, acc.3)
+            (acc.0, acc.1 + 1, acc.2, acc.3)
         } else if x < center_width && y > center_height {
-            (acc.0, acc.1, acc.2+1, acc.3)
+            (acc.0, acc.1, acc.2 + 1, acc.3)
         } else {
-            (acc.0, acc.1, acc.2, acc.3+1)
+            (acc.0, acc.1, acc.2, acc.3 + 1)
         }
     });
     return quadrants.0 * quadrants.1 * quadrants.2 * quadrants.3;
 }
 
-fn simulate_second(robots: &Vec<Robot>, second_num: i64, width: i64, height: i64) -> Vec<(i64, i64)> {
-    let positions: Vec<(i64, i64)> =  robots.iter().map(|robot| {
-        let x = (robot.initial_x + (second_num * robot.velocity_x)) % width;
-        let y = (robot.initial_y + (second_num * robot.velocity_y)) % height;
-        let x = if x < 0 {
-            x + width
-        } else {
-            x
-        };
-        let y = if y < 0 {
-            y + height
-        } else {
-            y
-        };
-        (x, y)
-    }).collect();
+fn simulate_second(
+    robots: &Vec<Robot>,
+    second_num: i64,
+    width: i64,
+    height: i64,
+) -> Vec<(i64, i64)> {
+    let positions: Vec<(i64, i64)> = robots
+        .iter()
+        .map(|robot| {
+            let x = (robot.initial_x + (second_num * robot.velocity_x)) % width;
+            let y = (robot.initial_y + (second_num * robot.velocity_y)) % height;
+            let x = if x < 0 { x + width } else { x };
+            let y = if y < 0 { y + height } else { y };
+            (x, y)
+        })
+        .collect();
     positions
 }
 
 fn parse_robots(input: &str) -> Vec<Robot> {
-    let robots: Vec<Robot> = input.lines().map(|line| {
-        let mut whitespace = line.split_whitespace();
-        let mut position_split = whitespace.next().unwrap().split("=");
-        position_split.next();
-        let mut position_values = position_split.next().unwrap().split(",");
-        let initial_x = position_values.next().unwrap().parse::<i64>().unwrap();
-        let initial_y = position_values.next().unwrap().parse::<i64>().unwrap();
-        let mut velocity_split = whitespace.next().unwrap().split("=");
-        velocity_split.next();
-        let mut velocity_values = velocity_split.next().unwrap().split(",");
-        let velocity_x = velocity_values.next().unwrap().parse::<i64>().unwrap();
-        let velocity_y = velocity_values.next().unwrap().parse::<i64>().unwrap();
-        Robot {
-            initial_x,
-            initial_y,
-            velocity_x,
-            velocity_y,
-        }
-    }).collect();
+    let robots: Vec<Robot> = input
+        .lines()
+        .map(|line| {
+            let mut whitespace = line.split_whitespace();
+            let mut position_split = whitespace.next().unwrap().split("=");
+            position_split.next();
+            let mut position_values = position_split.next().unwrap().split(",");
+            let initial_x = position_values.next().unwrap().parse::<i64>().unwrap();
+            let initial_y = position_values.next().unwrap().parse::<i64>().unwrap();
+            let mut velocity_split = whitespace.next().unwrap().split("=");
+            velocity_split.next();
+            let mut velocity_values = velocity_split.next().unwrap().split(",");
+            let velocity_x = velocity_values.next().unwrap().parse::<i64>().unwrap();
+            let velocity_y = velocity_values.next().unwrap().parse::<i64>().unwrap();
+            Robot {
+                initial_x,
+                initial_y,
+                velocity_x,
+                velocity_y,
+            }
+        })
+        .collect();
     robots
 }
 
@@ -85,9 +87,13 @@ fn print_locations(robots: &Vec<Robot>, second_num: i64, width: i64, height: i64
         dot_matrix[*y as usize][*x as usize] = '0';
     });
     let mut lines = vec![format!("{second_num}").to_string()];
-    lines.extend(dot_matrix.iter().map(|line| {line.iter().collect::<String>()}));
+    lines.extend(
+        dot_matrix
+            .iter()
+            .map(|line| line.iter().collect::<String>()),
+    );
 
-    return lines.join("\n")
+    return lines.join("\n");
 }
 
 struct Robot {
@@ -99,12 +105,14 @@ struct Robot {
 
 fn part2_with_input(input: &str) -> i64 {
     let robots = parse_robots(input);
-    let interesting_seconds: Vec<i64> = (20..100).map(|i| {
-        vec![86+(i*101), 154+(i*103)]
-    }).flatten().collect();
-    let drawings = interesting_seconds.into_par_iter().map(|second_num| {
-        print_locations(&robots, second_num, 101, 103)
-    }).collect::<Vec<String>>();
+    let interesting_seconds: Vec<i64> = (20..100)
+        .map(|i| vec![86 + (i * 101), 154 + (i * 103)])
+        .flatten()
+        .collect();
+    let drawings = interesting_seconds
+        .into_par_iter()
+        .map(|second_num| print_locations(&robots, second_num, 101, 103))
+        .collect::<Vec<String>>();
     let final_drawing = drawings.join("\n");
     fs::write("data/tree_finder.txt", final_drawing).expect("Unable to write file");
     return 0;
--- src/twentyfour/day15.rs
+++ src/twentyfour/day15.rs
@@ -2,8 +2,190 @@ use crate::create_advent_day;
 
 create_advent_day!("2024", "15");
 
-fn part1_with_input(_input: &str) -> i64 {
-    return 0;
+fn part1_with_input(input: &str) -> i64 {
+    let mut warehouse = parse_warehouse(input);
+    for direction in warehouse.directions.clone().iter() {
+        warehouse.march(*direction);
+    }
+    return warehouse.calculate_gps() as i64;
+}
+
+fn parse_warehouse(input: &str) -> Warehouse {
+    let mut map: Vec<Vec<char>> = Vec::new();
+    let mut directions: Vec<char> = Vec::new();
+    let mut found_empty = false;
+    let mut guard_x = 0_usize;
+    let mut guard_y = 0_usize;
+    for (line_num, line) in input.lines().enumerate() {
+        if line.is_empty() {
+            found_empty = true;
+            continue;
+        }
+        if !found_empty {
+            map.push(line.chars().collect());
+            if line.contains("@") {
+                let row_num = line.find("@").unwrap();
+                guard_x = row_num;
+                guard_y = line_num;
+            }
+        } else {
+            directions.extend(line.chars());
+        }
+    }
+    return Warehouse {
+        map,
+        directions,
+        guard_x,
+        guard_y,
+    };
+}
+
+struct Warehouse {
+    map: Vec<Vec<char>>,
+    directions: Vec<char>,
+    guard_x: usize,
+    guard_y: usize,
+}
+
+impl Warehouse {
+    fn _debug(&self) {
+        let lines = self
+            .map
+            .iter()
+            .map(|line| {
+                let value: String = line.iter().collect::<String>();
+                value
+            })
+            .collect::<Vec<String>>();
+        let print_me = lines.join("\n");
+        println!("{print_me}");
+    }
+
+    fn march(&mut self, direction: char) {
+        match direction {
+            '<' => self.march_left(),
+            '>' => self.march_right(),
+            '^' => self.march_up(),
+            'v' => self.march_down(),
+            _ => todo!("invalid direction"),
+        }
+    }
+
+    fn march_up(&mut self) {
+        let mut lookahead = (self.guard_x, self.guard_y);
+        let can_move: bool;
+        loop {
+            if self.map[lookahead.1][lookahead.0] == '.' {
+                can_move = true;
+                break;
+            } else if self.map[lookahead.1][lookahead.0] == '#' {
+                can_move = false;
+                break;
+            }
+            lookahead.1 -= 1;
+        }
+        if !can_move {
+            return;
+        }
+        self.map[self.guard_y][self.guard_x] = '.';
+        self.map[self.guard_y - 1][self.guard_x] = '@';
+        self.guard_y -= 1;
+        if lookahead.1 < self.guard_y {
+            self.map[lookahead.1][lookahead.0] = 'O';
+        }
+    }
+
+    fn march_down(&mut self) {
+        let mut lookahead = (self.guard_x, self.guard_y);
+        let can_move: bool;
+        loop {
+            if self.map[lookahead.1][lookahead.0] == '.' {
+                can_move = true;
+                break;
+            } else if self.map[lookahead.1][lookahead.0] == '#' {
+                can_move = false;
+                break;
+            }
+            lookahead.1 += 1;
+        }
+        if !can_move {
+            return;
+        }
+        self.map[self.guard_y][self.guard_x] = '.';
+        self.map[self.guard_y + 1][self.guard_x] = '@';
+        self.guard_y += 1;
+        if lookahead.1 > self.guard_y {
+            self.map[lookahead.1][lookahead.0] = 'O';
+        }
+    }
+
+    fn march_left(&mut self) {
+        let mut lookahead = (self.guard_x, self.guard_y);
+        let can_move: bool;
+        loop {
+            if self.map[lookahead.1][lookahead.0] == '.' {
+                can_move = true;
+                break;
+            } else if self.map[lookahead.1][lookahead.0] == '#' {
+                can_move = false;
+                break;
+            }
+            lookahead.0 -= 1;
+        }
+        if !can_move {
+            return;
+        }
+        self.map[self.guard_y][self.guard_x] = '.';
+        self.map[self.guard_y][self.guard_x - 1] = '@';
+        self.guard_x -= 1;
+        if lookahead.0 < self.guard_x {
+            self.map[lookahead.1][lookahead.0] = 'O';
+        }
+    }
+
+    fn march_right(&mut self) {
+        let mut lookahead = (self.guard_x, self.guard_y);
+        let can_move: bool;
+        loop {
+            if self.map[lookahead.1][lookahead.0] == '.' {
+                can_move = true;
+                break;
+            } else if self.map[lookahead.1][lookahead.0] == '#' {
+                can_move = false;
+                break;
+            }
+            lookahead.0 += 1;
+        }
+        if !can_move {
+            return;
+        }
+        self.map[self.guard_y][self.guard_x] = '.';
+        self.map[self.guard_y][self.guard_x + 1] = '@';
+        self.guard_x += 1;
+        if lookahead.0 > self.guard_x {
+            self.map[lookahead.1][lookahead.0] = 'O';
+        }
+    }
+
+    fn calculate_gps(&self) -> usize {
+        return self
+            .map
+            .iter()
+            .enumerate()
+            .map(|(line_num, line)| {
+                line.iter()
+                    .enumerate()
+                    .map(|(row_num, letter)| {
+                        if *letter != 'O' {
+                            0
+                        } else {
+                            (100 * line_num) + row_num
+                        }
+                    })
+                    .sum::<usize>()
+            })
+            .sum::<usize>();
+    }
 }
 
 fn part2_with_input(_input: &str) -> i64 {
@@ -15,15 +197,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_works() {
-        let actual = create("test.txt").solve_part1();
+    fn part1_1_works() {
+        let actual = create("test1.txt").solve_part1();
 
-        assert_eq!("0", &actual);
+        assert_eq!("10092", &actual);
+    }
+    #[test]
+    fn part1_2_works() {
+        let actual = create("test2.txt").solve_part1();
+
+        assert_eq!("2028", &actual);
     }
 
     #[test]
     fn part2_works() {
-        let actual = create("test.txt").solve_part2();
+        let actual = create("test1.txt").solve_part2();
 
         assert_eq!("0", &actual);
     }
